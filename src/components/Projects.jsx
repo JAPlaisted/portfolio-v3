0538ad8f0bdf2b51fa789447b32293bf2cf8e591
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2023-03-03 02:35:01 -0800

    Added project Github Links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ import ChatIcon from '@mui/icons-material/Chat';
 import CloudIcon from '@mui/icons-material/Cloud';
 import DataObjectIcon from '@mui/icons-material/DataObject';
 import SpatialTrackingIcon from '@mui/icons-material/SpatialTracking';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Projects(props) {
     return (
@@ -14,7 +15,12 @@ function Projects(props) {
                 <div className='project-row align-items-center justify-content-center'>
                     <a href="https://fastidious-sundae-5a2784.netlify.app/" target="/">
                         <div className='project' data-aos="fade-right">
-                            <PersonAddAlt1Icon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <PersonAddAlt1Icon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="https://github.com/JAPlaisted/sommet">
+                                        <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                    </a>
+                            </div>
                             <div>
                                 <h4>Sommet Technologies</h4>
                                 <p>Sommet Technologies is a software automation service that streamlines business processes. The React site has reusable components, browser router, and custom CSS.</p>
@@ -23,7 +29,12 @@ function Projects(props) {
                     </a>
                     <a href="https://fastidious-sundae-5a2784.netlify.app/blog/5b21ca3eeb7f6fbccd471815" target="/">
                         <div className='project' data-aos="fade-right">
-                            <ExtensionIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <ExtensionIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="/">
+                                        <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                    </a>
+                            </div>
                             <div>
                                 <h4>Bionic Font Plugin</h4>
                                 <p>The Bionic Font Plugin is a Chrome extension that converts browser fonts to Bionic Font, improving reading speed.</p>
@@ -34,7 +45,12 @@ function Projects(props) {
                 <div className='project-row align-items-center justify-content-center'>
                     <a href="https://superchat-7b816.web.app/" target="/">
                         <div className='project' data-aos="fade-right">
-                            <ChatIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <ChatIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="https://github.com/JAPlaisted/init-chat">
+                                    <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                </a>
+                            </div>
                             <div>
                                 <h4>Init Chat</h4>
                                 <p>InitChat is a chat app with fullstack capabilities using React.js and Firebase, featuring Google authentication and a banned users list.</p>
@@ -43,7 +59,12 @@ function Projects(props) {
                     </a>
                     <a href="https://famous-brioche-becb52.netlify.app/" target="/">
                         <div className='project' data-aos="fade-right">
-                            <CloudIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <CloudIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="https://github.com/JAPlaisted/weather-tile">
+                                            <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                        </a>
+                            </div>
                             <div>
                                 <h4>Weather Tile</h4>
                                 <p>Weather Tile is a React-based weather UI that uses nivo to create visually appealing visualizations for weather data.</p>
@@ -54,7 +75,12 @@ function Projects(props) {
                 <div className='project-row align-items-center justify-content-center'>
                     <a href="https://calm-figolla-4ac6c5.netlify.app/" target="/">
                         <div className='project' data-aos="fade-right">
-                            <DataObjectIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <DataObjectIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="https://github.com/JAPlaisted/rainy-data">
+                                            <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                        </a>
+                            </div>
                             <div>
                                 <h4>Rainy Data</h4>
                                 <p>Rainy Data is a React and Express web resource for downloading mock JS databases for front-end application development, using Axios.</p>
@@ -63,7 +89,12 @@ function Projects(props) {
                     </a>
                     <a href="/py-tracking-demo">
                         <div className='project' data-aos="fade-right">
-                            <SpatialTrackingIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                            <div>
+                                <SpatialTrackingIcon className='project-icon' style={{fontSize: "xx-large"}}/>
+                                <a href="https://github.com/JAPlaisted/facial-recognition">
+                                            <GitHubIcon className='project-icon' style={{fontSize: "xx-large"}} />
+                                        </a>
+                            </div>
                             <div>
                                 <h4>Python Tracking</h4>
                                 <p>This is my experimental space for working with Python object tracking scripts. I aim to use these scripts to develop more interactive web applications in the future.</p>
