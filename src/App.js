295b1812b0c3aff41d69ceed6aab685367859683
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2023-03-02 21:17:52 -0800

    Basic setup and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PyTracking from "./pages/PyTracking";
+import ArsDemo from "./pages/ArsDemo";
 import "./App.css";
 
 function App() {
-  return <div className="App">React Portfolio V3</div>;
+  return (
+    <div className="App" id="top">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/py-tracking-demo" element={<PyTracking />} />
+          <Route path="/ars-demo" element={<ArsDemo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ArsDemo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ArsDemo(props) {
+    return (
+        <div>
+            ARS Demo
+        </div>
+    );
+}
+
+export default ArsDemo;
